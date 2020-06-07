46f5ce7ff08b8c7997b596bf386cb06a17da00db
Amel Islamovic <aislqmovic@gmail.com>
2020-06-07 20:45:43 +0200

    Added Navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Navbar extends Component {
+  render() {
+    return (
+      <nav>
+        <h1>Sorting Visualization</h1>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
